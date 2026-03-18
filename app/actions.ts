676b86aa77cf41c664722bb97c42310a126fc500
Moulzo <kountamoulzo@gmail.com>
2026-03-18 12:06:20 +0000

    creation file actions.ts, add function checkAndAddUser

---
+++ app/actions.ts
@@ -0,0 +1,29 @@
+"use server"
+
+import prisma from "@/lib/prisma";
+
+export async function checkAndAddUSer(email: string, name: string) {
+    if (!email) return
+    try {
+        const existingUser = await prisma.user.findUnique({
+            where: {
+                email: email
+            }
+        })
+        if (!existingUser && name) {
+            await prisma.user.create({
+                data: {
+                    email,
+                    name
+                }
+            })
+            console.error("Erreur lors de la  vérification de l'utilisateur:");
+        }else{
+            console.error("Utilisateur déjà présent dans la base de données.");
+        }
+    } catch (error) {
+        console.error("Erreur lors de la vérification de l'utilisateur:", error);
+    }
+
+
+}
