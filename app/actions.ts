d49844b4fd4c627c0a5819828591e61cc44a6805
Moulzo <kountamoulzo@gmail.com>
2026-03-23 11:57:13 +0000

    feat: add task-related server actions

--- app/actions.ts
+++ app/actions.ts
@@ -336,3 +336,16 @@ export async function createTask(
     }
 
 }
+
+export async function deleteTaskById(taskId: string) {
+    try {
+        await prisma.task.delete({
+            where: {
+                id: taskId
+            }
+        })
+    } catch (error) {
+        console.error(error)
+        throw new Error
+    }
+}
